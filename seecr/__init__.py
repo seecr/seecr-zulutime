edacd2e4873c8070df54e9fc91a71f96d38c999b
Seecr Development Team <development@seecr.nl>
2014-06-10 15:00:17 +0200

    HM/TJ: open source license.

--- seecr/__init__.py
+++ seecr/__init__.py
@@ -1,10 +1,26 @@
 ## begin license ##
-# 
-# All rights reserved.
-# 
+#
+# Zulutime helps formatting and parsing timestamps.
+#
 # Copyright (C) 2011 Seek You Too B.V. (Seecr) http://www.seecr.nl
-# Copyright (C) 2012 Seecr (Seek You Too B.V.) http://seecr.nl
-# 
+# Copyright (C) 2012, 2014 Seecr (Seek You Too B.V.) http://seecr.nl
+#
+# This file is part of "Zulutime"
+#
+# "Zulutime" is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# "Zulutime" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with "Zulutime"; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
 ## end license ##
 
 from pkgutil import extend_path
